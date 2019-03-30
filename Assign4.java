16dd269e1e45efb08eb4325ea95f2d30f2ee1624
rapabapa <45604184+rapabapa@users.noreply.github.com>
2019-03-29 20:51:42 -0700

    Update Assign4.java

--- Assign4.java
+++ Assign4.java
@@ -16,3 +16,50 @@ public class Assign4
    }
 
 }
+
+public interface BarcodeIO {
+   //implamenting methods 
+	boolean scan(final BarcodeImage bc);
+	
+	boolean readText(final String test);
+	
+	boolean generateImageFromText();
+	
+	boolean translateImageToText();
+	
+	void displayTextToConsole();
+	
+	void displayImageToConsole();
+	
+}
+
+public class BarcodeImage implements Cloneable
+{
+	
+	public static final int MAX_HEIGHT = 30;
+	
+	public static final int MAX_WIDTH = 65;
+	
+	private boolean [][] imageData;
+	public BarcodeImage() {
+		imageData = new boolean[MAX_HEIGHT][MAX_WIDTH];
+		for(int x = 0; x < MAX_WIDTH; x++) {
+			for (int y = 0; y < MAX_HEIGHT; y++) {
+				setPixel(x, y, false);
+			}
+		}
+	}
+	public BarcodeImage(String[] strData) {
+		
+	}
+	
+	public boolean getPixel(int row, int col) {
+		return false;
+	}
+	
+	public boolean setPixel(int row, int col, boolean value) {
+		return false;
+	}
+	
+	
+}
